5243de3f34795cd330bf114f3d75610b298b62cf
Muthia Rahma Mursyidi <muthia.konsultan@work.bri.co.id>
2023-06-11 16:38:35 +0700

    init

---
+++ order-service/src/main/java/com/p79/orderservice/kafka/OrderProducer.java
@@ -0,0 +1,40 @@
+package com.p79.orderservice.kafka;
+
+// import org.apache.kafka.clients.admin.NewTopic;
+// import org.apache.kafka.common.protocol.Message;
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.kafka.core.KafkaTemplate;
+// import org.springframework.kafka.support.KafkaHeaders;
+// import org.springframework.messaging.support.MessageBuilder;
+// import org.springframework.stereotype.Service;
+
+
+// @Service
+// public class OrderProducer {
+
+// private static final Logger LOGGER = LoggerFactory.getLogger(OrderProducer.class);
+// private NewTopic topic;
+// private KafkaTemplate<String, OrderEventpackage> kafkaTemplate;
+
+// public OrderProducer(NewTopic topic, KafkaTemplate<String, OrderEvent> kafkaTemplate) {
+// this.topic = topic;
+// this.kafkaTemplate = kafkaTemplate;
+//     }
+
+// public void sendMessage(OrderEvent event, Message<OrderEvent> MessageBuilder){
+// LOGGER.info(String.format("Order event => %s", event.toString()));
+
+// //create message
+// Message<OrderEvent> message = MessageBuilder
+// .withPayload(event)
+// .setHeader(KafkaHeaders.TOPIC, topic.name())
+// .build();
+// kafkaTemplate.send(message);
+
+// }
+// }
+
+
+
+
